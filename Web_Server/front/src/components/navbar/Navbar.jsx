47eb5aa768ac7c453eead50865088efc03814d20
ozgemer-git <oz_gemer@outlook.com>
2023-04-09 17:48:20 +0300

    nav profile

--- Web_Server/front/src/components/navbar/Navbar.jsx
+++ Web_Server/front/src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavContainer } from './Navbar.styled';
 import Gear from '../../assets/icons/settings/settings.png';
 import GearAnimation from '../../assets/icons/settings/settings.gif';
+import Profile from '../../assets/icons/profile.png';
 import '../../assets/icons/settings/settings.css';
 
 function Navbar() {
@@ -9,6 +10,13 @@ function Navbar() {
     <NavContainer>
       <div className='nav-left'></div>
       <div className='nav-right'>
+        <img
+          className='nav-profile'
+          src={Profile}
+          alt='profile'
+          width={35}
+          height={35}
+        />
         <SettingsGear width={35} height={35} />
       </div>
     </NavContainer>
--- Web_Server/front/src/components/navbar/Navbar.styled.jsx
+++ Web_Server/front/src/components/navbar/Navbar.styled.jsx
@@ -1,13 +1,22 @@
 import styled from 'styled-components';
 
 const NavContainer = styled.div`
-  padding: 0 0.5rem;
   width: 100vw;
   min-height: 50px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   border-bottom: 1px solid black;
+  & * {
+    margin: 0 0.25rem;
+    cursor: pointer;
+  }
+  & .nav-profile {
+    transition: 0.25s ease-in-out;
+  }
+  & .nav-profile:hover {
+    transform: scale(1.1);
+  }
 `;
 
 export { NavContainer };
