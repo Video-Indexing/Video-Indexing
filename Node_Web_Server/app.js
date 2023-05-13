365dc2cded8ca14089d867efd276fb493808e6c0
shaharkozi <102664929+shaharkozi@users.noreply.github.com>
2023-05-13 21:27:11 +0300

    add mail functionality - need to fix

--- Node_Web_Server/app.js
+++ Node_Web_Server/app.js
@@ -1,8 +1,5 @@
+const mailer = require('./mailer.js');
 const express = require('express');
-// const { test } = require('node:test');
-// var firebaseService = import('./firebaseService.mjs');
-// import { test } from './firebaseService.mjs'
-// firebaseService.test();
 var httpService = require('./httpService');
 const cors = require('cors');
 const firebaseService = require('./firebaseConfig');
@@ -11,12 +8,12 @@ const { json } = require('stream/consumers');
 const axios = require('axios');
 
 require('dotenv').config()
-// console.log(process.env)
-// const path = require('path')
 const app = express();
 const PORT = 5050;
 app.use(express.json());
 app.use(bodyParser.json());
+
+const path = require('path');
 
 algoServerIP = '';
 algoPort = '';
@@ -33,14 +30,8 @@ else{
 app.listen(PORT, (error) =>{
     if(!error){
         console.log(`Server is Successfully Running in ${process.env.STATUS} mode, and App is listening on port ` + PORT);
-        // var obj = { name : "welcome" , link : "", indexing : ""}
-        // var json = JSON.stringify(obj);
-        // console.log(json);
-        // var obj2 = JSON.parse(json);
-        // firebaseService.createVideo(firebaseService.videoCollection,obj2).then(x => console.log( x ));
-        // firebaseService.searchVideo(firebaseService.videoCollection,"test_req_pro_2").then(
-        //     vids => console.log(JSON.stringify(vids))
-        // );
+        // console.log(__filename); // prints the absolute path of the current module
+        // console.log(path.basename(__filename));
     }
     else 
         console.log("Error occurred, server can't start", error);
@@ -54,29 +45,32 @@ app.post("/user", (req, res) => {
 
 //request from front to algo
 app.post("/uploadVideo", (req, res) => {
-    sendToAlgoServer(algoServerIP + ":" + algoPort , JSON.stringify(req.body));
+    if (Object.keys(req.body).length === 0) {
+        res.status(400).json({ error: 'Request body cannot be empty' });
+      } else {
+        res.status(200).send();
+        sendToAlgoServer(algoServerIP + ":" + algoPort , JSON.stringify(req.body));
+      }
 });
 
 //algo response
 app.post("/uploadVideoAlgo",(req, res) => {
-    const data = JSON.stringify(req.body); // Get JSON data from request body
-    const obj = JSON.parse(data);
-    console.log(obj)
-    firebaseService.createVideo(firebaseService.videoCollection, obj);
-    res.status(200).send();
-});
-
-app.get("/videoStatus", (req, res) => {
-    //req  parameters:  videoID
+    if (Object.keys(req.body).length === 0) {
+        res.status(400).json({ error: 'Request body cannot be empty' });
+      } else {
+        const data = JSON.stringify(req.body); // Get JSON data from request body
+        const obj = JSON.parse(data);
+        console.log(obj)
+        firebaseService.createVideo(firebaseService.videoCollection, obj);
+        res.status(200).send();
+        // mailer.sendMailToClient("obn2468@gmail.com","or", obj.name)
+        console.log("finished upload")
+      }
 });
 
 app.get("/video",(req,res) => {
     res.send(getAllVideos());
 });
-
-// function handleUplaodVideo(body, res){
-//     res.send(body);
-// }
 
 async function sendToAlgoServer(url, data) {
     axios.post(url, data)
