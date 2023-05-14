e3e3129344ec77325594f768d6bac0f6eacf8ff0
AngelGamesS <95039976+AngelGamesS@users.noreply.github.com>
2023-05-14 14:10:04 +0300

    Update UploadService.js

--- Web_Server/front/src/services/UploadService.js
+++ Web_Server/front/src/services/UploadService.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 const UploadVideo = async (name, link) => {
     let res;
-    let obj = {naem,link};
-    let jsonData = JSON.stringify(obj);
+    let obj = {name,link};
+    // let jsonData = JSON.stringify(obj);
     // console.log(jsonData);
-    await axios.post("http://127.0.0.1:5050/uploadVideo",jsonData).then(
+    await axios.post("http://127.0.0.1:5050/uploadVideo",obj).then(
         (r) => {
-            console.log(r.data);
+            // console.log(r.data);
             res= r;    
         }
     );
