e1330bdb1ee98b21d4f0c805ba01b828a2b1339b
ozgemer-git <oz_gemer@outlook.com>
2023-06-09 12:28:04 +0300

    design v3

--- Web_Server/front/src/services/UploadService.js
+++ Web_Server/front/src/services/UploadService.js
@@ -1,18 +1,24 @@
 import axios from 'axios';
 
 const UploadVideo = async (name, link) => {
-    let res;
-    let obj = {name,link};
-    // let jsonData = JSON.stringify(obj);
-    // console.log(jsonData);
-    await axios.post("http://127.0.0.1:5050/uploadVideo",obj).then(
-        (r) => {
-            // console.log(r.data);
-            res= r;    
-        }
-    );
-    // if()
-    return res;
+  let res;
+  let obj = { name, link };
+  // let jsonData = JSON.stringify(obj);
+  // console.log(jsonData);
+  await axios.post('http://127.0.0.1:5050/uploadVideo', obj).then((r) => {
+    // console.log(r.data);
+    res = r;
+  });
+  // if()
+  return res;
 };
 
-export {UploadVideo};
+const getVideoSubjects = async () => {
+  let res;
+  await axios.get('http://127.0.0.1:5050/getAllTopics').then((r) => {
+    // console.log(r.data);
+    res = r;
+  });
+};
+
+export { UploadVideo, getVideoSubjects };
